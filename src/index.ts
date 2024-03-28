3ef15147fa742655b60b593a2efd8ec4cdce9a6a
aoteron <aoteronunez@gmail.com>
2024-03-28 20:12:35 +0100

    initialize server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+// Importing the 'app' object from the './server' file.
 import app from './server';
 
-app.listen(3000, () => {
+// Starting the server to listen for incoming HTTP requests on port 4000.
+app.listen(4000, () => {
     
 })
