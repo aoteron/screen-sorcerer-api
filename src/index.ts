a73fe32707f87239d99548e471e14c6ddd91fcf6
aoteron <aoteronunez@gmail.com>
2024-03-28 22:37:38 +0100

    set environment variables
    add files, set gitignore, feat @config.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-// Importing the 'app' object from the './server' file.
 import app from './server';
+import config from './config/config';
+
+console.log(config.app.PORT)
+console.log('Funciono')
 
 // Starting the server to listen for incoming HTTP requests on port 4000.
 app.listen(4000, () => {
