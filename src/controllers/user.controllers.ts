670827336749d63f29c9b84be673a9f505d59d3b
aoteron <aoteronunez@gmail.com>
2024-05-07 18:20:28 +0200

    Server launched

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+export const getAllUsers = (req: Request, res: Response) => {
+    res.send("All users")
+}
+
+export const getUser = (req: Request, res: Response) => {
+    res.send("User info")
+}
+
+export const createUser = (req: Request, res: Response) => {
+    res.send("User successfully created")
+}
+
+export const updateUser = (req: Request, res: Response) => {
+    res.send("User updated")
+}
+
+export const deleteUser = (req: Request, res: Response) => {
+    const { userId } = req.params
+    res.send("User deleted")
+}
