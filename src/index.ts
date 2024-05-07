6ae81381c0c55435387cce6a35f488e5b8ecd8b2
aoteron <aoteronunez@gmail.com>
2024-05-07 09:23:10 +0200

    \u{21A9}\u{FE0F} reset project

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './server';
-import config from './config/config';
-
-const PORT = config.app.PORT
-
-// Starting the server to listen for incoming HTTP requests on port 4000.
-app.listen(PORT, () => console.log('Server listening on port ' + PORT))
