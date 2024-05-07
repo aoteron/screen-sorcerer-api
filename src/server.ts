36ead847ac21e6e041f53dff3d572c97f4fa24b0
aoteron <aoteronunez@gmail.com>
2024-05-07 09:23:10 +0200

    Reset project

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import userRouter from './routes/user.routes';
-
-const app = express();
-
-app.use(express.json());
-app.use('/user', userRouter)
-export default app;
