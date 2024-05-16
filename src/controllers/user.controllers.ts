3791018e0feb028ceb7c74363b84feab24e31311
aoteron <aoteronunez@gmail.com>
2024-05-16 14:26:08 +0200

    feat(api): Add CRUD operations for user management
    Implemented endpoints for creating, reading, updating, and deleting users in the API.

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,22 +1,64 @@
-import { Request, Response } from 'express'
+import { Request, Response, response } from 'express'
+import UserModel from '../models/user.model';
 
-export const getAllUsers = (req: Request, res: Response) => {
-  res.send('All users')
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const allUsers = await UserModel.find()
+    res.status(200).send(allUsers)
+  } catch(error) {
+    res.status(400).send(error)
+  }
 }
 
-export const getUser = (req: Request, res: Response) => {
-  res.send('User info')
+export const getUser = async (req: Request, res: Response) => {
+  const {userId} = req.params
+
+  try {
+    const specificUser = await UserModel.findById({ _id: userId })
+
+    if (!specificUser) {
+      return res.status(404).send('User not found')
+    }
+
+    res.status(200).send(specificUser);
+    } catch (error) {
+    res.status(400).send(error)
+  }
 }
 
-export const createUser = (req: Request, res: Response) => {
-  res.send('User successfully created')
+export const createUser = async (req: Request, res: Response) => {
+  const {name, email, password} = req.body
+  try {
+    const newUser = await UserModel.create({ name, email, password })
+    res.status(201).send(newUser)
+  } catch (error) {
+    res.status(400).send(error)
+  }
 }
 
-export const updateUser = (req: Request, res: Response) => {
-  res.send('User updated')
+export const updateUser = async (req: Request, res: Response) => {
+  const {name, email, password} = req.body
+  const {userId} = req.params
+
+  try {
+    const userUpdated = await UserModel.findByIdAndUpdate(
+      { _id: userId },
+      { name, email, password },
+      { new:true }
+    )
+    res.status(201).send(userUpdated)
+  } catch (error) {
+    res.status(400).send(error)
+  }
 }
 
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
   const { userId } = req.params
-  res.send('User deleted')
+  
+  try {
+    const userDeleted = await UserModel.findByIdAndDelete({ _id: userId})
+    res.status(204).send(userDeleted)
+  } catch (error) {
+    res.status(400).send(error)
+  }
 }
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,32 +1,39 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose'
 
 interface IUserSchema {
-    name: String,
-    email: String,
-    password: String,
-    createAt?: Date,
-    updateAt?: Date
+  name: String
+  email: String
+  password: String
+  createdAt?: Date
+  updatedAt?: Date
 }
 
-const userSchema = new Schema<IUserSchema> ({
+const userSchema = new Schema<IUserSchema>(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    createAt: {
-        type: String,
-        required: false
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
-}, { timestamps: true })
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    }
+  },
+  { timestamps: true },
+)
 
-const UserModel = model<IUserSchema>( 'User', userSchema )
+const UserModel = model<IUserSchema>('User', userSchema)
 
-export default UserModel;
+export default UserModel
